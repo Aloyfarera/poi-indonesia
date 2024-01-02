09c70f311e7416041b1b65c13c243b55eb67341f
Aloyfarera <79070641+Aloyfarera@users.noreply.github.com>
2024-01-02 17:53:08 +0700

    Update trans_snow_table.py

--- snowflake/trans_snow_table.py
+++ snowflake/trans_snow_table.py
@@ -3,7 +3,7 @@ import sf_key
 con = snowflake.connector.connect(**sf_key.snowflake_conn)
 cursor = con.cursor()
 print("---append to master snowflake tabel---")
-select_query = "COPY INTO POI_INDONESIA.PUBLIC.POI_INDONESIA_MASTER FROM @STG_SI_DEMO_STAGE_1 PATTERN = '.*csv' FILE_FORMAT =csv1 FORCE = True;"
+select_query = "COPY INTO POI_INDONESIA.PUBLIC.POI_INDONESIA_MASTER FROM @STG_SI_DEMO_STAGE_1 PATTERN = '.*csv' FILE_FORMAT =csv1 FORCE = False;"
 
 cursor.execute(select_query)
 con.commit()
